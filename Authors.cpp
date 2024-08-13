b400c8c495bd515be5e9105eb945cf1a5f8df2df
TemurJan <116508751+TemurJan@users.noreply.github.com>
2024-08-13 16:21:04 +0400

    Authors.cpp

--- Authors.cpp
+++ Authors.cpp
@@ -1,28 +1,51 @@
 #pragma once
-#include <vector>
+#include "Authors.h"
+#include <iostream>
 #include <string>
+#include <vector>
 
-class Authors
+Authors::Authors(std::vector<std::string> name) :name(std::move(name)) {}
+
+bool Authors::operator==(const Authors& other) const
 {
-private:
-    std::vector<std::string> name;
-public:
-    Authors(std::vector<std::string> name);
+    if (other.name.size() != name.size())
+        return false;
+    for (std::size_t i = 0; i < name.size(); ++i)
+        if (name[i] != other.name[i])
+            return false;
+    return true;
+}
 
-    Authors() = default;
+bool Authors::operator<(const Authors& other) const
+{
+    return name < other.name;
+}
 
-    bool operator==(const Authors& other) const;
+bool Authors::operator>(const Authors& other) const
+{
+    return name > other.name;
 
-    bool operator<(const Authors& other) const;
+}
 
-    bool operator>(const Authors& other) const;
+bool Authors::operator<=(const Authors& other) const
+{
+    return !(name > other.name);
+}
 
-    bool operator<=(const Authors& other) const;
+bool Authors::operator>=(const Authors& other) const
+{
+    return !(name < other.name);
+}
 
-    bool operator>=(const Authors& other) const;
+void Authors::printAuthors() const
+{
+    std::cout << "Authors: ";
+    for (const auto& elem : name)
+        std::cout << elem << ", ";
+    std::cout << "\n";
+}
 
-    void printAuthors() const;
-
-    std::vector<std::string> getName() const;
-};
-
+std::vector<std::string> Authors::getName() const
+{
+    return name;
+}
